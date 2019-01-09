4fcc8b8520ce69b069f2bdf575050178a7f81059
Jan Willem Keizer <janwillemkeizer@gmail.com>
2019-01-09 16:07:14 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$connection = mysqli_connect('localhost', 'root', 'WKWsJVZcemt7UHzp') or die
+$connection = mysqli_connect('localhost', 'root', 'root') or die
                 ("Fout met de verbinding naar de server.");
 mysqli_select_db($connection, 'dbLOI') or die
         ("Fout met de verbinding naar de database.");
